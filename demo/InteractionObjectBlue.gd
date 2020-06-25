98d67e06ee1c6efb4d2ba3d0651f71251ce3fd3c
Jacopo Farina <jacopo.farina@flixbus.com>
2020-06-25 19:36:25 +0200

    Better signal handling

--- demo/InteractionObjectBlue.gd
+++ demo/InteractionObjectBlue.gd
@@ -8,11 +8,11 @@ func _ready():
 	bib = get_node("/root/Main/BlockingInputBox")
 	
 func on_interact():
-	bdb.append_text("Hello", 4)
-	bdb.append_text("What's your name?[break ask_name]", 1)
-	bdb.connect("ask_name", self, "ask_name")	
+	bdb.append_text("Hello", 20)
+	bdb.append_text("What's your name?[break ask_name]", 10)
+	bdb.connect("break_reached", self, "ask_name")	
 
-func ask_name():
+func ask_name(_unused: String):
 	bib.ask_input()
 	bib.connect("text_entered", self, "when_name_inserted")	
 
